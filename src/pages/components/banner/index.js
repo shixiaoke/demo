5e9b42b47a907cc5215518815e6b9f7b04bd14fc
石小可 <1257280429@qq.com>
2021-06-22 01:00:31 +0800

    fixed

--- src/pages/components/banner/index.js
+++ src/pages/components/banner/index.js
@@ -1,23 +1,27 @@
 import React, { Fragment, useState, useEffect, useRef } from 'react';
 import styles from './index.less';
-import { getPhotos } from '../../models/server'
+import { getPhotos } from '../../models/server';
 
-const Index = ({dataSource, ...props}) => {
+const Index = ({ dataSource, ...props }) => {
   const scrollItem = useRef();
+  const [allWidth, setAllWidth] = useState(94);
   useEffect(() => {
-
-  }, [])
+    const temp =
+      scrollItem && scrollItem.current && scrollItem.current.offsetWidth;
+    setAllWidth(parseInt(temp * 0.3));
+  }, []);
 
   return (
     <div className={styles.bannerWrap}>
       <div className={styles.box}>
         <div className={styles.box_scroll} ref={scrollItem} id="scrollItem">
-          {dataSource.map(item => (
+          {dataSource.map((item) => (
             <div
               className={styles.box_img}
+              style={{ width: allWidth }}
               key={item.id}
             >
-              <img key={item.id} src={item.url}/>
+              <img key={item.id} src={item.url} />
             </div>
           ))}
         </div>
@@ -25,10 +29,13 @@ const Index = ({dataSource, ...props}) => {
       <div
         className={`${styles.btn} ${styles.prev}`}
         onClick={() => {
-          if(scrollItem.current.scrollLeft == 0) {
-            return
+          if (scrollItem.current.scrollLeft == 0) {
+            return;
           }
-          scrollItem.current.scrollLeft = scrollItem.current.scrollLeft - scrollItem.current.offsetWidth * 0.3 - 15;
+          scrollItem.current.scrollLeft =
+            scrollItem.current.scrollLeft -
+            scrollItem.current.offsetWidth * 0.3 -
+            15;
         }}
       >
         <div className={`${styles.btn_arrow}`}></div>
@@ -36,12 +43,13 @@ const Index = ({dataSource, ...props}) => {
       <div
         className={`${styles.btn} ${styles.next}`}
         onClick={() => {
-          scrollItem.current.scrollLeft = scrollItem.current.scrollLeft + scrollItem.current.offsetWidth * 0.3 + 15;
+          scrollItem.current.scrollLeft =
+            scrollItem.current.scrollLeft + allWidth + 15;
         }}
       >
         <div className={`${styles.btn_arrow}`}></div>
       </div>
     </div>
   );
-}
+};
 export default Index;
